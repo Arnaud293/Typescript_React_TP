6045f73c71f11f0461bf2c2f3c0f4a2512b5b366
Arnaud293 <arnaudlt293@gmail.com>
2023-01-10 14:44:26 +0100

    navigate to article details, add styles

--- src/components/PostsLists.tsx
+++ src/components/PostsLists.tsx
@@ -1,4 +1,5 @@
 
+import { useNavigate } from "react-router-dom";
 import { PostData } from "../interfaces";
 
 interface PostsListProps{
@@ -6,12 +7,15 @@ interface PostsListProps{
 }
 
 const PostsLists: React.FC <PostsListProps> = ({allPosts}) => {
+
+  const navigate = useNavigate()
+
   return (
     <ul className="w-full flex flex-wrap gap-2 mt-5">
         {allPosts?.map((post) => (
-            <li key={post.id} className='w-[300px] min-h-[200px] bg-gray-800 rounded-md p-3'>
+            <li key={post.id} className='w-[300px] min-h-[200px] bg-gray-800 rounded-md p-3' onClick={() => navigate(`/${post.id}`)}>
                 <h2 className="font-bold text-lg border-b text-white p-1">{post.title}</h2>
-                <p className="text-white">Read article</p>
+                <p className="text-white cursor-pointer transition duration-200 hover:text-purple-800">Read article</p>
             </li>
         ))}
     </ul>
