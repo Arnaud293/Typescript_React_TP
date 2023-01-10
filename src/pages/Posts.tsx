709fe1ae3e3f3d5e4c0d0a726209420c597617f7
Arnaud293 <arnaudlt293@gmail.com>
2023-01-10 14:43:51 +0100

    set/get numbers of posts to/from localstorage

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -8,27 +8,32 @@ const Posts: React.FC = () => {
     const [allPosts, setAllPosts] = useState<PostData[] | null>(null);
     const [postsQuantity, setPostsQuantity] = useState<number>(5);
 
+    const quantityFromLocalOrState = () => localStorage.getItem('number') || postsQuantity;
+
+    const getPostsQuantity = quantityFromLocalOrState();
+
     useEffect(() => {
         const getPosts = async () => {
-            const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${postsQuantity}`);
+            const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${getPostsQuantity}`);
             const data: PostData[] = await response.json();
             setAllPosts(data);
         }
         getPosts();
         
         
-    },[postsQuantity]);
+    },[getPostsQuantity]);
 
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setPostsQuantity(+e.target.value);
+        localStorage.setItem('number', e.target.value);
     }
 
     return (
         <div className="w-full flex items-center flex-col">
             <h1 className='mt-3'>Main</h1>
             <div className='flex flex-col w-[80%]'>
-                <label htmlFor="posts">Posts Numbers {postsQuantity}</label>
-                <input type="range" min={1} max={20} defaultValue={5} onChange={onChange}/>
+                <label htmlFor="posts">Posts Numbers {getPostsQuantity}</label>
+                <input type="range" min={1} max={20} defaultValue={getPostsQuantity} onChange={onChange}/>
                 <PostsLists allPosts={allPosts} />
             </div>
         </div>
